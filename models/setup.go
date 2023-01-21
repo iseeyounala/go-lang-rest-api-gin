913f1535a22a3a93a482ba1a95c65b15ce44754e
wongsatorn boonpeng <wongsatorn_fluck@yahoo.com>
2023-01-21 23:19:54 +0700

    create models product & controller product 100%

---
+++ models/setup.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(mysql.Open("root:root@tcp(localhost:8889)/go_restapi_gin"))
+	if err != nil {
+		panic(err)
+	}
+	database.AutoMigrate(&Product{})
+
+	DB = database
+}
